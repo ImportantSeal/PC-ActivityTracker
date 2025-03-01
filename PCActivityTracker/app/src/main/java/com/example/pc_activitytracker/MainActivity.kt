69fee4bcb81a7bc2220cea42f649abc3b39e8c2c
Topi Vuori <topitvuori@gmail.com>
2025-03-01 12:17:41 +0200

    fixed device token for firebase notification

--- PCActivityTracker/app/src/main/java/com/example/pc_activitytracker/MainActivity.kt
+++ PCActivityTracker/app/src/main/java/com/example/pc_activitytracker/MainActivity.kt
@@ -25,14 +25,15 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
-        FirebaseMessaging.getInstance().subscribeToTopic("pcStatus")
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Log.d("FCM", "Subscribed to topic 'pcStatus'")
-                } else {
-                    Log.e("FCM", "Failed to subscribe to topic 'pcStatus'")
-                }
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
             }
+            val token = task.result
+            Log.d("FCM", "FCM Registration token: $token")
+        }
+
     }
 
 }
